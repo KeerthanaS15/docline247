fce149d2ad06bd2c3a25924bfd0d8559e379b6c8
jeevitha10 <jeevithajayakumar10@gmail.com>
2022-01-29 10:08:07 +0530

    Resolved all the comments

--- docline247/urls.py
+++ docline247/urls.py
@@ -19,11 +19,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('core/', include('core.urls')),
     path('admin/', admin.site.urls),
     path('__debug__/', include('debug_toolbar.urls')),
-    path('core/', include('core.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
+# endif
